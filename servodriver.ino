5c4d1a228783959a3d6f631c020739b9eec19b9e
Benjamin Liu <benjaminliu_dev@outlook.com>
2026-03-30 20:45:56 -0700

    optimization

--- servodriver.ino
+++ servodriver.ino
@@ -1,52 +1,68 @@
-#include <Servo.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-Servo servo1;
-Servo servo2;
-Servo servo3;
+volatile uint16_t pulse[3] = {1500, 1500, 1500}; // µs
 
-String getToken(String data, char delimiter, int index) {
-  int found = 0;
-  int startIndex = 0;
-  int endIndex = -1;
-  int maxIndex = data.length() - 1;
-
-  for (int i = 0; i <= maxIndex && found <= index; i++) {
-    if (data.charAt(i) == delimiter || i == maxIndex) {
-      found++;
-      startIndex = endIndex + 1;
-      endIndex = (i == maxIndex) ? i + 1 : i;
-    }
-  }
-
-  if (found > index) {
-    return data.substring(startIndex, endIndex);
-  }
-  return "";
-}
+volatile uint8_t state = 0;
 
 void setup() {
-  servo1.attach(4);
-  servo2.attach(5);
-  servo3.attach(7);
-  Serial.begin(115200);
+  // Set PA1, PA2, PA3 as output
+  PORTA.DIRSET = PIN1_bm | PIN2_bm | PIN3_bm;
+
+  // TCB0: periodic interrupt
+  TCB0.CCMP = 20000; // 20ms @ 1MHz (prescaler used below)
+  TCB0.CTRLA = TCB_CLKSEL_DIV2_gc | TCB_ENABLE_bm;
+  TCB0.INTCTRL = TCB_CAPT_bm;
+
+  sei();
+}
+
+ISR(TCB0_INT_vect) {
+  TCB0.INTFLAGS = TCB_CAPT_bm;
+
+  static uint8_t servo = 0;
+
+  switch (state) {
+    case 0:
+      PORTA.OUTSET = PIN1_bm | PIN2_bm | PIN3_bm;
+      servo = 0;
+      state = 1;
+      TCB0.CCMP = pulse[0];
+      break;
+
+    case 1:
+      PORTA.OUTCLR = (1 << (servo + 1));
+      servo++;
+
+      if (servo < 3) {
+        TCB0.CCMP = pulse[servo];
+      } else {
+        TCB0.CCMP = 20000;
+        state = 0;
+      }
+      break;
+  }
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char buffer[16];
-    int len = Serial.readBytesUntil('\n', buffer, sizeof(buffer) - 1);
-    buffer[len] = '\0';
+  if (USART0.STATUS & USART_RXCIF_bm) {
+    char id = USART0.RXDATAL;
+    if (USART0.RXDATAL == ':') {
+      uint16_t val = 0;
+      char c;
 
-    int servo_id = atoi(strtok(buffer, ":"));
-    int value = atoi(strtok(NULL, ":"));
+      while (!(USART0.STATUS & USART_RXCIF_bm));
+      while ((c = USART0.RXDATAL) != '\n') {
+        if (c >= '0' && c <= '9')
+          val = val * 10 + (c - '0');
+        while (!(USART0.STATUS & USART_RXCIF_bm));
+      }
 
+      if (val < 1000) val = 1000;
+      if (val > 2000) val = 2000;
 
-    switch (servo_id) {
-      case 1: servo1.write(value); break;
-      case 2: servo2.write(value); break;
-      case 3: servo3.write(value); break;
+      if (id >= '1' && id <= '3')
+        pulse[id - '1'] = val;
     }
-
-    delay(20);
   }
 }
